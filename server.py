2f02738c2132314c8c33babbb8baa7f9a7744157
hmignon <84913892+hmignon@users.noreply.github.com>
2022-02-05 17:33:40 +0100

    bugfix places booked deducted from club points tests passed

--- server.py
+++ server.py
@@ -56,6 +56,7 @@ def purchase_places():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     places_required = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+    club['points'] = int(club['points']) - places_required
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
