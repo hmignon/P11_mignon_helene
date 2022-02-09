eb7ae5611a0ee2d1649a82bcc12ea1943a5f95cc
hmignon <84913892+hmignon@users.noreply.github.com>
2022-02-09 02:12:47 +0100

    reformat code
    migrated 'non-routes' to utils for clarity
    improved server.py (removed repeated code)

--- server.py
+++ server.py
@@ -1,60 +1,16 @@
-import json
 from datetime import datetime
 
 from flask import Flask, render_template, request, redirect, flash, url_for
 
-
-def load_clubs():
-    with open('clubs.json') as c:
-        list_of_clubs = json.load(c)['clubs']
-        return list_of_clubs
-
-
-def load_competitions():
-    with open('competitions.json') as comps:
-        list_of_competitions = json.load(comps)['competitions']
-        return list_of_competitions
-
-
-def sort_competitions_date(comps):
-    past = []
-    present = []
-
-    for comp in comps:
-        if datetime.strptime(comp['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
-            past.append(comp)
-        elif datetime.strptime(comp['date'], '%Y-%m-%d %H:%M:%S') >= datetime.now():
-            present.append(comp)
-
-    return past, present
-
-
-def initialize_booked_places(comps, clubs_list):
-    places = []
-    for comp in comps:
-        for club in clubs_list:
-            places.append({'competition': comp['name'], 'booked': [0, club['name']]})
-
-    return places
-
+import utils
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-competitions = load_competitions()
-clubs = load_clubs()
-past_competitions, present_competitions = sort_competitions_date(competitions)
-places_booked = initialize_booked_places(competitions, clubs)
-
-
-def update_booked_places(competition, club, places_required):
-    for item in places_booked:
-        if item['competition'] == competition['name']:
-            if item['booked'][1] == club['name'] and item['booked'][0] + places_required <= 12:
-                item['booked'][0] += places_required
-                break
-            else:
-                raise ValueError("You can't book more than 12 places in a competition.")
+competitions = utils.load_competitions()
+clubs = utils.load_clubs()
+past_competitions, present_competitions = utils.sort_competitions_date(competitions)
+places_booked = utils.initialize_booked_places(competitions, clubs)
 
 
 @app.route('/')
@@ -87,22 +43,18 @@ def book(competition, club):
     if found_club and found_competition:
         if datetime.strptime(found_competition['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
             flash("This competition is over.", 'error')
-            return render_template(
-                'welcome.html',
-                club=club,
-                past_competitions=past_competitions,
-                present_competitions=present_competitions
-            ), 403
-        return render_template('booking.html', club=found_club, competition=found_competition)
+        else:
+            return render_template('booking.html', club=found_club, competition=found_competition)
 
     else:
         flash("Something went wrong-please try again", 'error')
-        return render_template(
-            'welcome.html',
-            club=club,
-            past_competitions=past_competitions,
-            present_competitions=present_competitions
-        ), 403
+
+    return render_template(
+        'welcome.html',
+        club=club,
+        past_competitions=past_competitions,
+        present_competitions=present_competitions
+    ), 403
 
 
 @app.route('/purchasePlaces', methods=['POST'])
@@ -112,38 +64,37 @@ def purchase_places():
 
     try:
         places_required = int(request.form['places'])
+
+        if places_required > int(competition['numberOfPlaces']):
+            flash('Not enough places available.', 'error')
+
+        elif places_required > int(club['points']):
+            flash('You don\'t have enough points.', 'error')
+
+        elif places_required > 12:
+            flash('You can\'t book more than 12 places in a competition.', 'error')
+
+        else:
+            try:
+                utils.update_booked_places(competition, club, places_booked, places_required)
+                competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+                club['points'] = int(club['points']) - places_required
+                flash('Great-booking complete!', 'success')
+
+                return render_template(
+                    'welcome.html',
+                    club=club,
+                    past_competitions=past_competitions,
+                    present_competitions=present_competitions
+                )
+
+            except ValueError as error_message:
+                flash(error_message, 'error')
+
     except ValueError:
         flash('Please enter a number between 0 and 12.', 'error')
-        return render_template('booking.html', club=club, competition=competition), 403
 
-    if places_required > int(competition['numberOfPlaces']):
-        flash('Not enough places available.', 'error')
-        return render_template('booking.html', club=club, competition=competition), 403
-
-    elif places_required > int(club['points']):
-        flash('You don\'t have enough points.', 'error')
-        return render_template('booking.html', club=club, competition=competition), 403
-
-    elif places_required > 12:
-        flash('You can\'t book more than 12 places in a competition.', 'error')
-        return render_template('booking.html', club=club, competition=competition), 403
-
-    else:
-        try:
-            update_booked_places(competition, club, places_required)
-            competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
-            club['points'] = int(club['points']) - places_required
-            flash('Great-booking complete!', 'success')
-            return render_template(
-                'welcome.html',
-                club=club,
-                past_competitions=past_competitions,
-                present_competitions=present_competitions
-            )
-
-        except ValueError as error_message:
-            flash(error_message, 'error')
-            return render_template('booking.html', club=club, competition=competition), 403
+    return render_template('booking.html', club=club, competition=competition), 403
 
 
 # TODO: Add route for points display
